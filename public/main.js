457d64dc797357551d8b0e1fad97ebbc8b73d36e
marie <marie.nguyen.phu.qui@gmail.com>
2019-05-20 10:52:29 +1200

    first vue component for home page

--- public/main.js
+++ public/main.js
@@ -1,47 +1,36 @@
-// Vue.component('home', {
-//   template: `<!-- Home page on click will render list of super heros par Universe-->
-//   <div>
-//     <div class="universe">
-//         <a :href="link"><h1>{{universe}}</h1></a>
-//         <a :href="universe"></a><img class="universe-img" :src="image">
-//     </div>
+Vue.component('home', {
 
-//     <!-- on click will render supers heros specific characteristics component-->
-//     <div class="super-list">
-//         <!-- will be displaying img of superheros google img search https://www.googleapis.com/customsearch/v1?key=YOUR_API_KEY&cx=YOUR_CSE_ID&q={{superhero}}&searchType=image&fileType=jpg&imgSize=small&alt=json -->
-//         <!-- src= google api.link -->
-//         <img src="">
-//         <a :href="superhero"><li>{{superhero}}</li></a>
-//     </div>
-    
-//     <!-- last route with details per one heros-->
-//     <div class="super">
-//       <h1>{{superhero}}</h1>
-//       <p>
-//           {{publisher}} 
-//           {{alter_ego}}
-//           {{first_appearance}}
-//           {{characters}}
-//       </p>
-//     </div>
-//   <div>`,
-//   data() { return {
-//     universe: 'DC',
-//     image: '../data/img/DC.jpg',
-//     link:'https://www.dccomics.com/',
-//     superhero: 'SUPER',
-//     publisher: 'super',
-//     alter_ego: 'super',
-//     first_appearance: 'super',
-//     characters:'super'
-//   }},
-//   // methods : ,
-//   computed: {
-//     title() {
-//       return this.superhero
-//     }
-//   }
-// })
+  template: `
+  <div id="homepage">
+    <div v-for="key in universe" class="universe">
+    <!-- on click display chosen universe name - will redirect to page -->
+    <button :id="key.name" v-on:click="displayUniverse">{{key.name}}</button>
+    <h1>You are in {{chosen_universe}}</h1>
+    <a :href="universe"></a><img class="universe-img" :src="key.image">
+    </div>
+  </div>
+  `,
+  data() { 
+    return {
+      universe : {
+        marvel : {
+          name : 'MARVEL',
+          image: '../data/img/MARVEL.jpg',
+          super_list: '../data/marvel.json'
+        },
+        DC : {
+          name: 'DC',
+          image: '../data/img/DC.jpg',
+          super_list: '../data/dc.json'
+        }
+      },
+  }},
+  methods : {
+    displayUniverse : function(e) {
+      this.chosen_universe = e.target.id
+    },
+  }
+})
 
 
 
